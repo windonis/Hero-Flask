97ec7fd769b227072c80e2ef211cbf61c8423f4b
Enis Kollugil <enis.kollugil@hepsiburada.com>
2020-05-25 13:45:37 +0300

    do it simple

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 
 # We establish a Flask route so that we can serve HTTP traffic on that route 
 @app.route('/')
-def person():
-    with open('./data.json', 'r') as jsonfile:
-        file_data = json.loads(jsonfile.read())
-    return Response(json.dumps(file_data), mimetype='application/json')
-
+def index():
+    #with open('./data.json', 'r') as jsonfile:
+    #    file_data = json.loads(jsonfile.read())
+    #return Response(json.dumps(file_data), mimetype='application/json')
+    return "selam"
 # Get setup so that if we call the app directly (and it isn't being imported elsewhere)
 # it will then run the app with the debug mode as True
 # More info - https://docs.python.org/3/library/__main__.html
